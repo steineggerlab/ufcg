f6ee164e2bd43c34771d29e1d60c0664c9d92416
DW Kim <endix1029@gmail.com>
2023-01-02 11:29:06 +0900

    v1.0.3 conda ver

--- src/pipeline/UFCGMainPipeline.java
+++ src/pipeline/UFCGMainPipeline.java
@@ -36,7 +36,7 @@ import sun.misc.Signal;
 
 @SuppressWarnings("restriction")
 public class UFCGMainPipeline {
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.0.3";
 	public static final String RELEASE_DATE = "Jan. 2023";
 	public static final String CITATION = " Kim, D., Gilchrist, C.L.M., Chun, J. & Steinegger, M.\n"
 										+ " UFCG: database of universal fungal core genes and pipeline for genome-wide phylogenetic analysis of fungi.\n"
